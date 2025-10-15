3843fce0588e5655f2ed9ae800d2479b388ef0f9
Boris Okner <boris.okner@gmail.com>
2025-10-15 14:45:09 -0400

    Minor

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ClusterMap.MixProject do
 
   def project do
     [
-      app: :cluster_map,
+      app: :packer,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
